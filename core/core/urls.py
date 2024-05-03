1a738ee829a280261a1c95cf3098d4b998c53601
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-05-04 00:49:17 +0530

    Lecture 3 finished

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home.views import home, success_page, templates
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',home, name="home"),
+    path('success-page/',success_page,name="success_page"),
+    path('template/',templates,name="templates")
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return render(request,r'home/templates/index.html')
+
+def success_page(request):
+    return HttpResponse("<h1>This is a success page.</h1>")
+
+def templates(request):
+    return render(request,"index.html")
