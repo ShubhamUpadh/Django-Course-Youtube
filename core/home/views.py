bc6982e0a5d054e4c74b90c8fcb93a39d915dbb2
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-05-05 23:39:24 +0530

    Lecture 3 finished

--- core/home/views.py
+++ core/home/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,r'home/templates/index.html')
+    people =[
+        {"name":"Shubham","age":28},
+        {"name":"Rohan","age":28},
+        {"name":"Vicky Kaushal","age":23},
+        {"name":"Sandeep","age":21},
+        {"name":"Randeep","age":26}
+        
+    ]
+    return render(request,r'home/templates/index.html',context={'people':people})
 
 def success_page(request):
     return HttpResponse("<h1>This is a success page.</h1>")
